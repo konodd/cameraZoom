429fa52cbbd053974e5a300944809788cbd39b21
DW <DW@KONOSUBA>
2018-06-19 19:40:06 +0800

    2018.6.19

--- 摄像头变焦/test.cpp
+++ 摄像头变焦/test.cpp
@@ -1,93 +1,167 @@
-#include <opencv2\opencv.hpp>
-#include <opencv2\tracking\tracker.hpp>
-#include <string>
-#include <vector>
+#include"opencv2/core/core.hpp"
+#include"opencv2/highgui/highgui.hpp"
+#include"opencv2/imgproc/imgproc.hpp"
+#include<iostream>
+#include<queue>
+#include<thread>
+#include<mutex>
+#include<condition_variable>
+#include<time.h>
 
-int fps()
+using namespace std;
+using namespace cv;
+
+Mat cylinder(Mat imgIn, int f);
+Point2i getOffset(Mat img, Mat img1);
+Mat linearStitch(Mat img, Mat img1, Point2i a);
+
+mutex mut;
+queue<Point2i> data_queue;
+condition_variable data_cond;
+
+VideoCapture cap1(0);
+VideoCapture cap2(1);
+Mat frame1;
+Mat frame2;
+Mat frame;
+Point2i a;//存储偏移量
+bool run = true;
+double t = (double)getTickCount();//线程2时间
+double t1 = (double)getTickCount();//线程1时间
+void data_preparation_thread()
 {
-	static int fps = 0;
-	static int lastTime = cv::getTickCount();
-	static int frameCount = 0;
-
-	++frameCount;
-
-	int curTime = cv::getTickCount();
-	if ((curTime - lastTime) / cv::getTickFrequency() > 1.0) // 取固定时间间隔为1秒
+	while (run)
 	{
-		fps = frameCount;
-		frameCount = 0;
-		lastTime = curTime;
+		if (cap1.read(frame1) && cap2.read(frame2))
+		{
+			waitKey(1);
+			if (waitKey(20) >= 0)
+			{
+				run = false;
+			}
+			imshow("cam1", frame1);
+			imshow("cam2", frame2);
+
+			//彩色帧转灰度
+			cvtColor(frame1, frame1, CV_RGB2GRAY);
+			cvtColor(frame2, frame2, CV_RGB2GRAY);
+
+			//匹配
+			//cout << "正在匹配..." << endl;
+			t1 = ((double)getTickCount() - t1) / getTickFrequency();
+			cout << "匹配时间：" << t1 << endl;
+			t1 = (double)getTickCount();
+			a = getOffset(frame1, frame2);
+		}
+
+		lock_guard<mutex> lk(mut);
+		data_queue.push(a);
+		data_cond.notify_one();
+
 	}
-	return fps;
 }
 
-int main(int argc, char **argv)
+void data_processing_thread()
 {
-	cv::CascadeClassifier cas("E:/ROS/xl320_driver/haarcascades/haarcascade_frontalface_alt.xml");  //级联检测
-	cv::VideoCapture cap(0);
-	std::vector<cv::Rect> face_boxes;                                              //人脸框
-	cv::Rect2d tracking_box;                                                       //跟踪目标
-	cv::Mat frame;
-	cv::Mat result;
-	cv::Ptr<cv::TrackerKCF> face_tracker;                                          //KCF跟踪
-	cv::TrackerKCF::Params face_tracker_params;                                    //KCF跟踪参数
-	face_tracker_params.detect_thresh = 0.2;                                       //KCF跟踪置信度
-	face_tracker_params.resize = true;
-	bool isCenter = false, autoTrack = false;
-	char str[20];
-	bool is_detected = false;
-
-	while (cap.isOpened())
+	while (run)
 	{
-		cap >> frame;
-		if (frame.empty())
+		unique_lock<mutex> lk(mut);
+		data_cond.wait(lk, [] {return !data_queue.empty(); });
+		Point2i a = data_queue.front();
+		data_queue.pop();
+		lk.unlock();
+		//cout << "正在拼接"<<endl;
+		frame = linearStitch(frame1, frame2, a);
+		waitKey(20);
+		if (waitKey(20) >= 0)
 		{
-			continue;
+			run = false;
 		}
-		//检测
-		if (!is_detected)
-		{
-			cv::putText(frame, "detecting face", cv::Point(0, 20), cv::FONT_HERSHEY_COMPLEX, 0.6, cv::Scalar(255, 0, 0));
-			cas.detectMultiScale(frame, face_boxes);
-			//检测到目标后，初始化跟踪模块
-			if (!face_boxes.empty())
-			{
-				tracking_box = face_boxes[0];
-				face_tracker = cv::TrackerKCF::create(face_tracker_params);
-				face_tracker->init(frame, tracking_box);
-				is_detected = true;
-			}
-		}
-		//跟踪
-		else
-		{
-			if (face_tracker->update(frame, tracking_box))
-			{
-				result = frame(tracking_box);
-				cv::resize(result, result, cv::Size(tracking_box.width * 1.5, tracking_box.height * 1.5));
-				cv::imshow("face", result);
-				cv::putText(frame, "tracking face", cv::Point(0, 20), cv::FONT_HERSHEY_COMPLEX, 0.6, cv::Scalar(255, 0, 0));
-				cv::rectangle(frame, tracking_box, cv::Scalar(0, 0, 255));
-				cv::putText(frame, "tracking", cv::Point(tracking_box.x, tracking_box.y), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 255));
-			}
-			else
-			{
-				face_tracker.release();
-				is_detected = false;
-			}
-		}
-		//显示图片及按键操作
-		sprintf(str, "FPS:%d", fps());
-		cv::putText(frame, str, cv::Point(540, 30), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(255, 0, 0));
-		cv::imshow("test", frame);
-		int opt = cv::waitKey(10);
-		switch (opt)
-		{
-		case 'q':
-			return 1;
-		default:
-			break;
-		}
+		t = ((double)getTickCount() - t) / getTickFrequency();
+		cout << "拼接时间：" << t << endl;
+		imshow("stitch", frame);
+		t = (double)getTickCount();
+
 	}
+}
+
+int main()
+{
+	//初始化
+	if (cap1.isOpened() && cap2.isOpened())
+	{
+		cout << "*** ***" << endl;
+		cout << "摄像头已启动！" << endl;
+	}
+	else
+	{
+		cout << "*** ***" << endl;
+		cout << "警告：请检查摄像头（数量2）是否安装好!" << endl;
+		cout << "程序结束！" << endl << "*** ***" << endl;
+		return -1;
+	}
+	/*cap1.set(CV_CAP_PROP_FOCUS, 0);
+	cap2.set(CV_CAP_PROP_FOCUS, 0);*/
+
+	//匹配对齐线程
+	thread t1(data_preparation_thread);
+	//拼接线程
+	thread t2(data_processing_thread);
+	t1.join();
+	t2.join();
+
 	return 0;
+}
+
+Point2i getOffset(Mat img, Mat img1)
+{
+	Mat templ(img1, Rect(0, 0.4*img1.rows, 0.2*img1.cols, 0.2*img1.rows));
+	Mat result(img.cols - templ.cols + 1, img.rows - templ.rows + 1, CV_8UC1);//result存放匹配位置信息
+	matchTemplate(img, templ, result, CV_TM_CCORR_NORMED);
+	normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+	double minVal; double maxVal; Point minLoc; Point maxLoc; Point matchLoc;
+	minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+	matchLoc = maxLoc;//获得最佳匹配位置
+	int dx = matchLoc.x;
+	int dy = matchLoc.y - 0.4*img1.rows;//右图像相对左图像的位移
+	Point2i a(dx, dy);
+	return a;
+}
+
+
+Mat linearStitch(Mat img, Mat img1, Point2i a)
+{
+	int d = img.cols - a.x;//过渡区宽度
+	int ms = img.rows - abs(a.y);//拼接图行数
+	int ns = img.cols + a.x;//拼接图列数
+	Mat stitch = Mat::zeros(ms, ns, CV_8UC1);
+	//拼接
+	Mat_<uchar> ims(stitch);
+	Mat_<uchar> im(img);
+	Mat_<uchar> im1(img1);
+
+	if (a.y >= 0)
+	{
+		Mat roi1(stitch, Rect(0, 0, a.x, ms));
+		img(Range(a.y, img.rows), Range(0, a.x)).copyTo(roi1);
+		Mat roi2(stitch, Rect(img.cols, 0, a.x, ms));
+		img1(Range(0, ms), Range(d, img1.cols)).copyTo(roi2);
+		for (int i = 0; i < ms; i++)
+			for (int j = a.x; j < img.cols; j++)
+				ims(i, j) = uchar((img.cols - j) / float(d)*im(i + a.y, j) + (j - a.x) / float(d)*im1(i, j - a.x));
+
+	}
+	else
+	{
+		Mat roi1(stitch, Rect(0, 0, a.x, ms));
+		img(Range(0, ms), Range(0, a.x)).copyTo(roi1);
+		Mat roi2(stitch, Rect(img.cols, 0, a.x, ms));
+		img1(Range(-a.y, img.rows), Range(d, img1.cols)).copyTo(roi2);
+		for (int i = 0; i < ms; i++)
+			for (int j = a.x; j < img.cols; j++)
+				ims(i, j) = uchar((img.cols - j) / float(d)*im(i, j) + (j - a.x) / float(d)*im1(i + abs(a.y), j - a.x));
+	}
+
+
+	return stitch;
 }
